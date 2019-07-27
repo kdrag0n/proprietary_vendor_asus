012fc51bae86f93f7141ea377baeda2ac6987a94
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-07-27 12:15:00 +0200

    I01WD: Add aptX encoders and aptX adaptive
    Change-Id: I8e7a6179395c5e43b139dc0d52484c72d2fa8291
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- I01WD/I01WD-vendor.mk
+++ I01WD/I01WD-vendor.mk
@@ -73,6 +73,8 @@ PRODUCT_COPY_FILES += \
     vendor/asus/I01WD/proprietary/lib64/lib-imsvt.so:system/lib64/lib-imsvt.so \
     vendor/asus/I01WD/proprietary/lib64/lib-imsvtextutils.so:system/lib64/lib-imsvtextutils.so \
     vendor/asus/I01WD/proprietary/lib64/lib-imsvtutils.so:system/lib64/lib-imsvtutils.so \
+    vendor/asus/I01WD/proprietary/lib64/libaptXHD_encoder.so:system/lib64/libaptXHD_encoder.so \
+    vendor/asus/I01WD/proprietary/lib64/libaptX_encoder.so:system/lib64/libaptX_encoder.so \
     vendor/asus/I01WD/proprietary/lib64/libdiag_system.so:system/lib64/libdiag_system.so \
     vendor/asus/I01WD/proprietary/lib64/libdpmctmgr.so:system/lib64/libdpmctmgr.so \
     vendor/asus/I01WD/proprietary/lib64/libdpmfdmgr.so:system/lib64/libdpmfdmgr.so \
@@ -96,6 +98,7 @@ PRODUCT_PACKAGES += \
     libantradio \
     vendor.qti.hardware.fm@1.0 \
     QtiTelephonyService \
+    aptxals \
     CNEService \
     HotwordEnrollmentOKGoogleWCD9340 \
     HotwordEnrollmentXGoogleWCD9340 \
